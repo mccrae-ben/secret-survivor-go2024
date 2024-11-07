d591be9e992c2cf4afc0a548036bdbf4882e61a9
jvella <jeremie.vella@gmail.com>
2024-11-06 16:59:46 -0800

    shooting

--- godot/entities/player/player.gd
+++ godot/entities/player/player.gd
@@ -4,8 +4,15 @@ class_name Player
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
+var is_shooting = false
+
+@export var player_weapon: Weapon
+
 @onready var health_component: HealthComponent = $HealthComponent
-	
+@onready var sprite: Sprite2D = $Sprite2D
+
+func _ready() -> void:
+	player_weapon.shooting.connect(on_shoot)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,7 +28,24 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	if !is_shooting: 
+		if sign(direction.x) == -1 or Input.is_action_just_released("move_left") or get_global_mouse_position().x < position.x:
+			sprite.flip_h = true
+		
+		if sign(direction.x) == 1 or Input.is_action_just_released("move_right") or get_global_mouse_position().x > position.x:
+			sprite.flip_h = false	
+	
+	if is_shooting:
+		if get_global_mouse_position().x < position.x:
+			sprite.flip_h = true
+
+		if get_global_mouse_position().x > position.x:
+			sprite.flip_h = false
+		
 	
 	move_and_slide()
-
 	
+func on_shoot():
+	is_shooting = true
+		
--- godot/entities/player/wand_weapon.gd
+++ godot/entities/player/wand_weapon.gd
@@ -1,11 +1,16 @@
 extends Node2D
+class_name Weapon
 
 @export var bullet: PackedScene
+@export var max_angle: float = 30.0
 
 var can_shoot: bool = true
 var go_parent: Node2D
+
 @onready var muzzle_marker: Marker2D = %Marker2D
 @onready var fire_timer: Timer = $Timer
+
+signal shooting(shooting: bool)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,9 +21,13 @@ func _ready() -> void:
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	look_at(get_global_mouse_position())
+	#-35, 55
+	#var clamped_weapon_rotation = clamp(rotation,-35,55)
+	var target: Vector2 = get_global_mouse_position() - position
+	look_at(target)  # Rotate to face the target
 	
 	if can_shoot and Input.is_action_pressed("shoot"):
+		shooting.emit(true)
 		can_shoot = false
 		fire_timer.start()
 		var bullet_instance = bullet.instantiate() as Bullet
@@ -26,6 +35,9 @@ func _process(delta: float) -> void:
 		bullet_instance.global_position = muzzle_marker.global_position
 		bullet_instance.bullet_target = global_position - get_global_mouse_position()
 		bullet_instance.rotation = (global_position - get_global_mouse_position()).angle()
+	
+	else:
+		shooting.emit(false)
 	
 func on_fire_rate_timer_timeout():
 	can_shoot = true
