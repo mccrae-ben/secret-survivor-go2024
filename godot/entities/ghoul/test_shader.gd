e9bc6e621e78256c7bcf207da7a3d7e64057ef2e
benmccrae <146896997+mccrae-ben@users.noreply.github.com>
2024-11-07 18:57:03 -0800

    Update test_shader.gd

--- godot/entities/ghoul/test_shader.gd
+++ godot/entities/ghoul/test_shader.gd
@@ -13,31 +13,46 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if can_flash_backward:
-		flash_hit_backward()
+		flash_hit_from_front()
 		
 	if can_flash_forward:
-		flash_hit_forward()	
+		flash_hit_from_back()
 	
 func on_hit(_hit_dir: Vector2):
-	#material.set_shader_parameter("reset_color", false)
+	material.set_shader_parameter("reset_color", false)
 	updated_hit_point = _hit_dir
-	material.set_shader_parameter("hit_point", updated_hit_point)
-	print(updated_hit_point,"hit signal")
-	can_flash_backward = true
+	material.set_shader_parameter("hit_point", updated_hit_point) 
+	if _hit_dir.x > 0:
+		can_flash_backward = true
+	else:
+		can_flash_forward = true
 	
-func flash_hit_backward():
+func flash_hit_from_front():
 	updated_hit_point.x -= .05
 	updated_hit_point.y -= .05
+	print(updated_hit_point.y)
 	material.set_shader_parameter("hit_point", updated_hit_point)
-	print("mat",material.get("shader_parameter/hit_point"))
-#	if updated_hit_point == Vector2(-1,-1):
-#		updated_hit_point.x += .05
-#		updated_hit_point.y += .05	
-#		material.set_shader_parameter("hit_point", updated_hit_point)
 	
-	if updated_hit_point.x == 0 or updated_hit_point.x == 1:
+	if updated_hit_point.x <= -1:
 		material.set_shader_parameter("reset_color", true)
+		can_flash_backward = false
 	#print("mat",material.get("shader_parameter/hit_point"))
+
+func flash_hit_from_back():
+	updated_hit_point.x += .05
+	updated_hit_point.y += .05
+	material.set_shader_parameter("hit_point", updated_hit_point)
+	#print("mat",material.get("shader_parameter/hit_point"))
+	#	if updated_hit_point == Vector2(-1,-1):
+	#		updated_hit_point.x += .05
+	#		updated_hit_point.y += .05	
+	#		material.set_shader_parameter("hit_point", updated_hit_point)
+
+	if updated_hit_point.x >= 1:
+		material.set_shader_parameter("reset_color", true)
+		can_flash_backward = false
+#print("mat",material.get("shader_parameter/hit_point"))
+
 
 func flash_hit_forward():
 	if updated_hit_point.x < 1 and updated_hit_point.x >= 0:
